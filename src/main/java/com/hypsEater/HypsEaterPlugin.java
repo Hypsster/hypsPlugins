86078a95ae6b6ecab916acc4cabd0aff3274b169
Hypsster <115741249+Hypsster@users.noreply.github.com>
2023-06-15 22:58:22 -0400

    FInally got everything together.

--- src/main/java/com/hypsEater/HypsEaterPlugin.java
+++ src/main/java/com/hypsEater/HypsEaterPlugin.java
@@ -1,22 +1,17 @@
 package com.hypsEater;
 
 import com.example.InteractionApi.InventoryInteraction;
+import com.example.InventoryUtils.InventoryUtils;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.GameTick;
-import net.runelite.api.events.StatChanged;
-import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import com.example.InventoryUtils.InventoryChecker;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @PluginDescriptor(
@@ -32,29 +27,19 @@ public class HypsEaterPlugin extends Plugin {
 	@Inject
 	private ItemManager itemManager;
 
-
-	String[] menuActions = {"Eat"};
-
 	@Subscribe
 	public void onGameTick(GameTick gameTick) {
 		int hp = client.getBoostedSkillLevel(Skill.HITPOINTS);
 
-		if (InventoryChecker.inventoryContainsEdibleFoods())
+		if (InventoryUtils.inventoryContainsEdibleFoods())
 		{
-			int foodToEat = InventoryChecker.findItemWithEatOption();
+			int foodToEat = InventoryUtils.findItemWithEatOption();
 			System.out.println(foodToEat);
 
 			InventoryInteraction.useItem(foodToEat,"Eat");
 		}else{
 			System.out.println("No Edibles");
 		}
-	}
-
-	private boolean hasEatOption(String menuAction) {
-			return menuAction != null && menuAction.contains("Eat");
-		}
-	@Subscribe
-	public void onStatChanged(StatChanged statChanged) {
 	}
 
 	@Provides
