c850dfd299e05bf996b0909db850d4fd0bf696dd
Hypsster <115741249+Hypsster@users.noreply.github.com>
2023-06-15 22:38:52 -0400

    FInally got everything together.

--- src/main/java/com/hypsEater/HypsEaterPlugin.java
+++ src/main/java/com/hypsEater/HypsEaterPlugin.java
@@ -46,7 +46,7 @@ public class HypsEaterPlugin extends Plugin {
 
 			InventoryInteraction.useItem(foodToEat,"Eat");
 		}else{
-			System.out.println("No Edibles");
+			System.out.println("No Ediles");
 		}
 	}
 
